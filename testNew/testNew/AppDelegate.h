ad11bfb9509c49df57e30a52a35c755c6831218d
Hello_World <pro@prodeMacBook-Pro.local>
2017-08-17 14:06:23 +0800

    cc
    第一次创建工程开始。

---
+++ testNew/testNew/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testNew
+//
+//  Created by pro on 17/8/17.
+//  Copyright © 2017年 pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
